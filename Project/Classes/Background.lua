5822fdbfc00781ee69408d4ff64b540cc1189e7a
JoanStark <47462893+JoanStark@users.noreply.github.com>
2019-11-05 15:18:51 +0100

    Update Background.lua

--- Project/Classes/Background.lua
+++ Project/Classes/Background.lua
@@ -3,7 +3,6 @@ local Background = Actor:extend()
 
 function Background:new()
   Background.super.new(self, "Resources/map1.jpeg",0,0,nil,nil,nil,0,0)
-  --image,x,y,speed,fx,fy, ox, oy, sx,sy
 end
 
 function Background:update(dt)
